8990ded0f865e5f18205e1c0cdc75008f41ad4e7
brianmarti1994 <brianmarti1994@gmail.com>
2020-03-12 10:16:17 +0300

    DbScript Update FK added

--- DbScript_DB_Tables.sql
+++ DbScript_DB_Tables.sql
@@ -10,6 +10,13 @@ GO
 CREATE DATABASE VehicleDb;
 
 
+USE [VehicleDb]
+GO
+/****** Object:  Table [dbo].[VehicleMake]    Script Date: 12/03/2020 10:11:19 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[VehicleMake](
 	[Id] [int] NOT NULL,
 	[Name] [nvarchar](50) NULL,
@@ -20,7 +27,7 @@ CREATE TABLE [dbo].[VehicleMake](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[VehicleModel]    Script Date: 19/02/2020 23:06:13 ******/
+/****** Object:  Table [dbo].[VehicleModel]    Script Date: 12/03/2020 10:11:20 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -36,6 +43,8 @@ CREATE TABLE [dbo].[VehicleModel](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  StoredProcedure [dbo].[SpDeleteVehicle]    Script Date: 19/02/2020 23:06:13 ******/
-SET ANSI_NULLS ON
+ALTER TABLE [dbo].[VehicleModel]  WITH CHECK ADD  CONSTRAINT [FK_VehicleMake_Id] FOREIGN KEY([MakeId])
+REFERENCES [dbo].[VehicleMake] ([Id])
+GO
+ALTER TABLE [dbo].[VehicleModel] CHECK CONSTRAINT [FK_VehicleMake_Id]
 GO
